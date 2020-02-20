e4837ee7b7a4fe7065bca1138a2c86657767fc1d
Sam Chen <sam.chen@geospock.com>
2020-02-20 17:00:40 +0000

    Solved A with a mathematically deterministic approach.

--- src/main/kotlin/io/hsar/practice3/ProblemA.kt
+++ src/main/kotlin/io/hsar/practice3/ProblemA.kt
@@ -1,19 +1,5 @@
 package io.hsar.practice3
 
-/**
- * Returns a pair. The boolean indicates whether it was success, the list is the results.
- */
-fun testPairwiseSums(pairwiseSums: List<Int>, valA: Int): Pair<Boolean, Array<Int>> {
-    val valB = pairwiseSums[0] - valA
-    val valC = pairwiseSums[2] - valB
-
-    return if ((pairwiseSums[1] - valC) == valA) {
-        true to arrayOf(valA, valB, valC)
-    } else {
-        false to emptyArray()
-    }
-}
-
 fun main() {
     val input = readLine()!!.split(" ")
             .map { inputSegment ->
@@ -25,11 +11,16 @@ fun main() {
     val pairwiseSums = input.minus(total)
             .sorted()
 
-    val searchLimit = (total / 3) + 1 // the furthest we will need to search is one-third of the total
-    for (valA in 1..searchLimit) {
-        val (result, values) = testPairwiseSums(pairwiseSums, valA)
-        if (result) {
-            println("${values[0]} ${values[1]} ${values[2]}")
-        }
-    }
+    val pairwiseDiffA = pairwiseSums[1] - pairwiseSums[0]
+    val pairwiseDiffB = pairwiseSums[2] - pairwiseSums[1]
+    val pairwiseDiffC = pairwiseSums[2] - pairwiseSums[0]
+
+    // Sum of these pairwise differences yields 2C-2A
+    val CminusA = (pairwiseDiffA + pairwiseDiffB + pairwiseDiffC) / 2
+    val CplusA = pairwiseSums[1]
+
+    val valC = (CplusA + CminusA) / 2
+    val valA = CplusA - valC
+    val valB = pairwiseSums[0] - valA
+    println("${valA} ${valB} ${valC}")
 }
